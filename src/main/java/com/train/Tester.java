d4ed1758c441195b1d22abb18932c82300b1a680
ggier9456 <ggier9456@gmail.com>
2021-07-07 23:13:19 +0800

    作業 2 - 連續訂票功能

--- src/main/java/com/train/Tester.java
+++ src/main/java/com/train/Tester.java
@@ -4,12 +4,36 @@ import java.util.Scanner;
 
 public class Tester {
     public static void main(String[] args) {
-        Scanner sca =new Scanner(System.in);
-        System.out.println("Please enter number of tickets: ");
-        int num= sca.nextInt();
-        System.out.println("How many round-trip tickets: ");
-        int rnum=sca.nextInt();
-        Tickets tic =new Tickets(num,rnum);
-        tic.print();
+        Scanner sca = new Scanner(System.in);
+        int num1=0;
+        int rnum1= 0;
+        int num = 0;
+        int rnum =0;
+
+        while (num != -1) {
+            System.out.println("Please enter number of tickets: ");
+            num = sca.nextInt();
+            num1 =num1+num;
+            if (num == -1) {
+                System.out.println("close down.");
+                Tickets tic = new Tickets(num1, rnum1);
+                tic.print();
+            } else{
+                System.out.println("How many round-trip tickets: ");
+                rnum = sca.nextInt();
+                rnum1=rnum1+rnum;
+
+            }
+            if(rnum1>num1){
+                System.out.println("Your number of round-trip tickets can't exceed your tickets\n Plz return again.");
+                num1=0;
+                rnum=0;
+                rnum1=0;
+            }
+            else{
+                Tickets tic = new Tickets(num1, rnum1);
+                tic.print();
+            }
+        }
+        }
     }
-}
--- src/main/kotlin/com/train/TesterKotlin.kt
+++ src/main/kotlin/com/train/TesterKotlin.kt
@@ -1,21 +1,39 @@
 package com.train
 
-import java.util.*
+
 
 
 fun main(args:Array<String>) {
-    val sca =Scanner(System.`in`)
-    println("Please enter number of tickets: ")
-    val num=sca.nextInt()
-    println("How many round-trip tickets: ")
-    val rnum=sca.nextInt()
-    val tic =Ticket(num,rnum)
-    tic.print()
 
+    var num: Int = 0
+    var num1: Int = 0
+    var rnum: Int = 0
+    var rnum1: Int = 0
+
+    while (num != -1) {
+        println("Please enter number of tickets: ")
+        num = readLine()!!.toInt();
+        num1 = num1 + num
+        if (num == -1) {
+            println("close down.")
+            val tic = Ticket(num1, rnum1)
+            tic.print()
+        } else {
+            println("How many round-trip tickets: ")
+            rnum = readLine()!!.toInt();
+            rnum1 = rnum1 + rnum
+        }
+        if(rnum1>num1){
+            println("Your number of round-trip tickets can't exceed your tickets\nPlz return again.")
+            rnum=0
+            num1=0
+            rnum1=0
+        }else{
+            val tic = Ticket(num1, rnum1)
+            tic.print()
+        }
+    }
 }
-
-
-
 class Ticket(var num:Int,var rnum:Int) {
     fun print() {
         println("tickets:"+num+"\n"+"round trip: "+rnum+"\n"+"total" + ((num - rnum) * 1000 + (rnum * 0.9 * 2000)))
